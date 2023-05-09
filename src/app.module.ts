4fd55fbf254971c23c5ce2b96507e3b559612cbd
Ilbert130 <ilbertca27@gmail.com>
2023-05-09 15:31:33 -0400

    Creating schema pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+
+    //Expedir contenido estico
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
+
+    //Importando la base de datos, para usarla, colocamos la cadena de conexion
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+    //Modelos de mi api
     PokemonModule
   ],
   controllers: [],
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,22 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+//Para determinar que es una entidad en mongoose
+@Schema()
+export class Pokemon extends Document{
+
+    //Agregando validaciones
+    @Prop({
+        unique:true,
+        index:true
+    })
+    name:string;
+
+    @Prop({
+        unique:true,
+        index:true
+    })
+    no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
